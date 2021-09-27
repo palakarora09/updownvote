87d10d67610003470123e76332c7a4b603944bf5
Palak Arora <68844163+palakarora09@users.noreply.github.com>
2021-09-27 15:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var counter = 3;
+
+function upvote() {
+ counter  = counter + 1;
+
+ document.getElementById("votes").innerHTML = counter + " votes";
+}
+
+function downvote() {
+ counter  = counter - 1;
+
+ document.getElementById("votes").innerHTML = counter + " votes";
+}
